b04e1850cfc403ed003cd25f07333a6394593e1e
いかろちゃん <aaxa2108@chiba-u.jp>
2015-10-04 12:09:05 +0900

    1章にFlaskの使い方を追加

--- test/app.py
+++ test/app.py
@@ -4,9 +4,11 @@ from flask import Flask,request
 
 app = Flask(__name__)
 
-@app.route('/calc',methods=['GET'])
+@app.route('/')
 def hello_world():
-    return request.args.get("name")
+    who = request.args.get("name")
+    return who
 
 if __name__ == '__main__':
     app.run(debug=True)
+
