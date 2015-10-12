ad51586efe3e2074938e69be0dd716103ff24e8d
いかろちゃん <aaxa2108@chiba-u.jp>
2015-10-13 00:08:52 +0900

    2章のif文の説明を追加

--- test/app.py
+++ test/app.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+# coding: UTF-8
 
 from flask import Flask,request
 
@@ -6,8 +7,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    who = request.args.get("who")
-    return who
+    age = int(request.args.get("age"))
+
+    if age < 10:
+        return "10歳未満なんですね"
+    elif age < 18:
+        return "18歳未満なんですね"
+    else:
+        return "18歳以上だからって特に何もありませんよ?"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
