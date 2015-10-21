7707314fbdb724e7b66a5363cc427e9f60fd7e19
いかろちゃん <aaxa2108@chiba-u.jp>
2015-10-21 14:36:40 +0900

    クラスを3章から移動したり

--- test/app.py
+++ test/app.py
@@ -1,12 +1,4 @@
 #!/bin/python3
 # coding: UTF-8
 
-import hashlib
-import time
-
-salt = str(time.time())
-passwd = 'pass'
-text=(passwd+salt).encode('utf-8')
-
-result = hashlib.sha512(text).hexdigest()
-print('insert into Users(name,pass_hash,salt) values(test,'+result+','+salt)
+hoge["foo"]
