ec958473b629f673fd5aa8c9b248bcb169155834
ikaro <exi_cosx_isinx@yahoo.co.jp>
2015-11-17 22:55:52 +0900

    テンプレートについて追記

--- test/app.py
+++ test/app.py
@@ -1,42 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from flask import Flask, redirect ,request
+from flask import Flask, redirect ,request,render_template
 
 app = Flask(__name__)
 
-@app.route("/",methods=['POST'])
+@app.route("/")
 def index():
-    who = request.args.get("name")
-    passwd = request.args.get("pass")
-    return str(who)+" "+str(passwd)
-
-@app.route("/login")
-def login():
-    html= """
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <title>ログイン</title>
-        <link rel="stylesheet" type="text/css" href="{{url_for('static', filename='style.css')}}" />
-        <!--&#91;if lt IE 9&#93;>
-        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-        <!&#91;endif&#93;-->
-    </head>
-    <body>
-
-        <form action="/" method="post">
-            <h1>Login</h1>
-            <input type="text" name="name" placeholder="Username" required/>
-            <input type="password" name="pass" placeholder="Password" required/>
-            <button>Login</button>
-        </form>
-
-    </body>
-    </html>
-    """
-    return html
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
--- test/myfuncs.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-# coding: UTF-8
-
-def add(a, b):
-    return a+b
-
-def subtract(a, b):
-    return a-b
