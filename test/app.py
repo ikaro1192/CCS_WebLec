28f1283327884d3d7e2c3f2c13ad1c6303fc3dcf
ikaro <exi_cosx_isinx@yahoo.co.jp>
2015-11-18 14:17:10 +0900

    4章完成

--- test/app.py
+++ test/app.py
@@ -5,9 +5,15 @@ from flask import Flask, redirect ,request,render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/check")
+def check():
+    return render_template('check.html')
 
-if __name__ == "__main__":
+@app.route('/',methods=['POST'])
+def index():
+    my_age = int(request.form["age"])
+    my_name = request.form["name"]
+    return render_template('index.html',age=my_age,name=my_name)
+
+if __name__ == '__main__':
     app.run(debug=True)
