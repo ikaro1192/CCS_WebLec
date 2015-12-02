c14c1b11078a41c6e50a28c7caa413d5b77cf0a8
ikaro <exi_cosx_isinx@yahoo.co.jp>
2015-12-02 15:59:38 +0900

    名前付きパラメータの話を追加

--- test/app.py
+++ test/app.py
@@ -4,6 +4,15 @@
 from sqlite3 import connect,Row
 
 con = connect('test.db')
+#取得する形式をディクショナリに
+con.row_factory = Row
+
+book_name='ModernC++Design'
+
 cur = con.cursor()
-cur.execute('insert into Books(name,price) values("test",500)')
-con.commit()
+cur.execute('select * from Books where name=:book_name or id >1',{'book_name':book_name})
+books = cur.fetchall()
+for book in books:
+    print(book['name']+"の価格は"+str(book['price'])+"です")
+
+
